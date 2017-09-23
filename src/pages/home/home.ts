1d0470bc68a2e0d561533a8ba19da8cbd645bbec
corey-johnson <corey.johnson777@yahoo.com>
2017-09-23 02:25:38 -0400

    Project Completion
    Project has been complete to specs.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {AlertController, NavController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -9,70 +9,70 @@ export class HomePage {
 
   cards = [
       {
-          imgPath:  "animeImg1.png",
+          imgPath:  "assets/img/animeImg1.png",
           title:    "My Hero Academia Season 3",
           desc:     "Fan Forum!",
           favorited: false,
           liked:     false
       },
       {
-          imgPath:  "animeImg2.png",
+          imgPath:  "assets/img/animeImg2.png",
           title:    "Parasyte: The Maxim Season 2",
           desc:     "Fan Theory!",
           favorited: false,
           liked:     false
       },
       {
-          imgPath:  "animeImg3.jpg",
+          imgPath:  "assets/img/animeImg3.jpg",
           title:    "Attack on Titan Season 2",
           desc:     "Recap and Review!",
           favorited: false,
           liked:     false
       },
       {
-          imgPath:  "animeImg4.jpg",
+          imgPath:  "assets/img/animeImg4.jpg",
           title:    "One Punch Man Season 2",
           desc:     "Expectations and Hopes!",
           favorited: false,
           liked:     false
       },
       {
-          imgPath:  "animeImg5.jpg",
+          imgPath:  "assets/img/animeImg5.jpg",
           title:    "Garden of Words",
           desc:     "Design and Art in Anime Forum!",
           favorited: false,
           liked:     false
       },
       {
-          imgPath:  "animeImg6.jpg",
+          imgPath:  "assets/img/animeImg6.jpg",
           title:    "Your Lie in April",
           desc:     "Music and Emotions in Anime Forum!",
           favorited: false,
           liked:     false
       },
       {
-          imgPath:  "animeImg7.jpg",
+          imgPath:  "assets/img/animeImg7.jpg",
           title:    "Tokyo Ghoul Season 3",
           desc:     "Info, Expectations & Book Vs. Anime Forum!",
           favorited: false,
           liked:     false
       },
       {
-          imgPath:  "animeImg8.jpg",
+          imgPath:  "assets/img/animeImg8.jpg",
           title:    "Gangsta. Season 2",
-          desc:     "Possibility in the wake of the bankruptcy of Manglobe Studio!",
+          desc:     "Possibilities in the wake of the bankruptcy of Manglobe Studio!",
           favorited: false,
           liked:     false
       },
       {
-          imgPath:  "animeImg9.jpg",
+          imgPath:  "assets/img/animeImg9.jpg",
           title:    "Durarara!! Season 2",
           desc:     "DVD and Blu-Ray Confirm Release Date!",
           favorited: false,
           liked:     false
       },
       {
-          imgPath:  "animeImg10.jpg",
+          imgPath:  "assets/img/animeImg10.jpg",
           title:    "Bleach",
           desc:     "Possible Restart to Anime!",
           favorited: false,
@@ -80,8 +80,108 @@ export class HomePage {
       }
   ];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl:AlertController) {
 
   }
+
+    clickFav(card) {
+        let index = this.cards.indexOf(card);
+
+        if(index > -1){
+            if(card.favorited){
+                this.cards[index].favorited = false;
+            }else{
+                this.cards[index].favorited = true;
+            }
+        }
+    }
+
+    clickLike(card){
+        let index = this.cards.indexOf(card);
+
+        if(index > -1){
+            if(card.liked){
+                this.cards[index].liked = false;
+            }else{
+                this.cards[index].liked = true;
+            }
+        }
+    }
+    addCard(){
+        let prompt = this.alertCtrl.create({
+            title: "Add Forum",
+            inputs: [
+                {
+                    name:"imgPath", placeholder:"Enter Image URL (Optional)"
+                },
+                {
+                    name:"title",   placeholder:"Enter Title of Forum"
+                },
+                {
+                    name:"desc",    placeholder:"Enter Description of Forum"
+                }
+            ],
+            buttons: [
+                {
+                    text: "Abort"
+                },
+                {
+                    text: "Confirm",
+                    handler: data => {
+                        console.log(data.imgPath);
+
+                        if(data.imgPath == ""){
+                            data.imgPath = "assets/img/imgPlaceHolder.png"
+                        }
+
+                        this.cards.push(data)
+                    }
+                }
+            ]
+        });
+        prompt.present();
+    }
+
+    editCard(cards) {
+        let prompt = this.alertCtrl.create({
+            title: "Edit Forum",
+            inputs: [
+                {
+                    name:"imgPath", placeholder:"Enter Image URL (Optional)"
+                },
+                {
+                    name:"title",   placeholder:"Enter Title of Forum"
+                },
+                {
+                    name:"desc",    placeholder:"Enter Description of Forum"
+                }
+            ],
+            buttons: [
+                {
+                    text: "Abort"
+                },
+                {
+                    text: "Confirm",
+                    handler: data => {
+                        console.log(data);
+                        if(data.imgPath == ""){
+                            data.imgPath = "assets/img/imgPlaceHolder.png"
+                        }
+                        let index = this.cards.indexOf(cards);
+                        if (index > -1){
+                            this.cards[index] = data;
+                        }
+                    }
+                }
+            ]
+        });
+        prompt.present();
+    }
+
+    eraseCard(cards) {
+        let index = this.cards.indexOf(cards);
+
+        this.cards.splice(index, 1)
+    }
 
 }
